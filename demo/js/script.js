f9e381d07ec897e1ec5b0172e07df88cc76bc23b
Fred <arnoux.frederic@gmail.com>
2012-04-10 16:20:24 +0200

    Maj demo with display values.

--- demo/js/script.js
+++ demo/js/script.js
@@ -4,10 +4,22 @@ $(function() {
   $('#demo-1').multiSlider({total: 2});
 
   // Demo 2
-  var demo2Slider = $('#demo-2').multiSlider({total: 1});
+  $('#demo-2').multiSlider({
+    total: 2,
+    slide: function (event, ui) {
+      $('#demo-2-values').html(ui.values.join(', '));
+    }
+  });
+
+  // Demo 3
+  var demo2Slider = $('#demo-3').multiSlider({total: 1});
   
-  $('#demo-2-select').change(function () {
+  $('#demo-3-select').change(function () {
     demo2Slider.option('total', $(this).val());
   });
     
+
+/*multiSlider.option('slide', function (event, ui) {
+  $('#demo-3-values').html(formatTimeDisplay(ui.values));
+});*/
 });
